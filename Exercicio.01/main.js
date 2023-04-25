11da3a2b4b6ff3f9f46760952233ef08540ade7f
Prates <guipratesdsantos@gmail.com>
2023-04-25 11:26:27 -0300

    calcular imc rev

--- Exercicio.01/main.js
+++ Exercicio.01/main.js
@@ -1,23 +1,23 @@
 function Calcular() {
     const form = document.querySelector('.form')
 
-    form.onsubmit = function(evento) {
+    form.onsubmit = function(evento) { //previnir atualização do formulario
         evento.preventDefault()
     }
-    const dadoPeso = document.getElementById('input-teste-1').value
-    const dadoAlt = document.getElementById('input-teste-2').value
-    let span = document.querySelector('.span')
+    const dadoPeso = document.getElementById('input-teste-1').value //valor do peso
+    const dadoAlt = document.getElementById('input-teste-2').value //valor da altura
+    let conclusao = document.querySelector('.conclusao') //texto-resultado do formulario
     
 
     let peso = Number(dadoPeso)
     let altura = Number(dadoAlt)
-    let imc = peso / (altura * altura)
+    let imc = peso / (altura * altura) //vlaor do imc
 
-    if(dadoPeso !== Number || dadoAlt !== Number){
+    if(dadoPeso !== Number || dadoAlt !== Number){ //aviso ao usuário de preenchimento incorreto do formulário
         span.innerHTML = 'Por favor, preencher os dois campos acima com números (peso e altura)'
     }
     
-    if(imc < 18.5){
+    if(imc < 18.5){ //análise do IMC do usuário
         span.innerHTML = `Seu IMC é de ${imc.toFixed(1)}. Você está abaixo do peso.`
     } else if(imc <= 24.9){
         span.innerHTML = `Seu IMC é de ${imc.toFixed(1)}. Seu peso está normal.`
